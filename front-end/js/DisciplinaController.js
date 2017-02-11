0df7085496d73a118df1036eac0161335ec7d05b
thiagohtorres <thiagohtorres@outlook.com>
2017-02-11 18:07:39 -0300

    front-end

---
+++ front-end/js/DisciplinaController.js
@@ -0,0 +1,20 @@
+app.controller('DisciplinaController',function($scope)
+{
+	//$scope.disciplina={};
+    $scope.listaDisciplinas = [];
+
+
+	$scope.gravar = function()	{
+		$scope.listaDisciplinas.push($scope.disciplina);
+		$scope.disciplina = {};
+	}
+
+	$scope.remover = function(disciplina)	{
+		$scope.listaDisciplinas.splicer($scope.listaDisciplinas.indexOf(),1);
+
+
+	}
+
+
+
+});
