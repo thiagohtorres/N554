26da54239af6a20b43702ee131b3df2f438e0759
thiagohtorres <thiagohtorres@outlook.com>
2017-02-23 20:17:42 -0300

    Exercicio 3

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,1 +1,19 @@
-var app = angular.module('SistemaAcademico',[]);
+var app = angular.module('SistemaAcademico',['ngRoute']);
+
+app.config(['$routeProvider',
+      function($routeProvider) {
+    $routeProvider.
+      when('/cadastro_disciplina', {
+        templateUrl: 'disciplina.html',
+        controller: 'DisciplinaController'
+      }).
+      when('/cadastro_turma', {
+        templateUrl: 'turma.html',
+        controller: 'TurmaController'
+      }).
+      when('/cadastro_aluno', {
+        templateUrl: 'aluno.html',
+        controller: 'AlunoController'
+      });
+}]);
+
