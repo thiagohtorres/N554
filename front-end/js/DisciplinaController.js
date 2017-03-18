d952e612ea1486d56a0c15aa3ae27866d4f656d3
thiagohtorres <thiagohtorres@outlook.com>
2017-03-18 11:19:19 -0300

    atualizado

--- front-end/js/DisciplinaController.js
+++ front-end/js/DisciplinaController.js
@@ -1,13 +1,8 @@
-app.controller('DisciplinaController',function($scope)
+app.controller('DisciplinaController',function($scope, $http)
 {
 	//$scope.disciplina={};
     $scope.listaDisciplinas = [];
-
-
-	$scope.gravar = function()	{
-		$scope.listaDisciplinas.push($scope.disciplina);
-		$scope.disciplina = {};
-	}
+	
 
 	$scope.remover = function(disciplina)	{
 
@@ -15,6 +10,43 @@ app.controller('DisciplinaController',function($scope)
 
 
 	}
+
+
+	$scope.gravar =function(){
+
+
+		$http.post('http://localhost:8080/ExemploRest/rest/disciplinas', $scope.disciplina).success(
+
+			function(dados){
+				$scope.disciplina = {};
+				$scope.listar();
+				alert(dados);
+
+			}
+
+
+			);
+
+	}
+
+
+
+
+	$scope.listar =function(){
+
+
+		$http.get('http://localhost:8080/ExemploRest/rest/disciplinas').success(
+
+			function(dados){
+				$scope.listaDisciplinas = dados;
+			}
+
+
+			);
+
+	}
+
+	$scope.listar();
 
 
 
